f1875673b3c9f86c6e572236f0886679b1fdf3de
Slava <rtootik@gmail.com>
2017-04-23 11:02:23 +0300

    Initial Commit

---
+++ 15-HomeWork-Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  15-HomeWork-Gestures
+//
+//  Created by Slava on 23.04.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
